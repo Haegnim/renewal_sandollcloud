26c77ae5f6c30a9695743c5fa3128d7a0d96b7b8
Haegnim <haegnim@gmail.com>
2021-06-24 18:31:29 +0900

    0630 아이콘 삽입/ 헤더 애니메이션/ 전체 레이아웃 수정

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+// --------- Header top animation ----------------
+function headerAni(){
+  const header = document.querySelector('#header');
+  let headerTop = header.top;
+  console.log(header);  
+
+  document.addEventListener('scroll', function() {
+  let winTop = document.documentElement.scrollTop;
+  console.log(winTop);
+  const headerTP = 65;
+  console.log(headerTP);
+
+    if(winTop >= headerTP){
+      header.classList.add('sticky');
+    } else {
+      header.classList.remove('sticky');
+    }
+  });
+}
+headerAni();
+
